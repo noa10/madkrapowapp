b987579bff689f7498d9affa96f923bc517a1f9f
noa10 <k.anwarbakar@gmail.com>
2025-03-14 13:27:53 +0800

    remove unused files

--- fix_social_accounts.sql
+++
@@ -1,8 +0,0 @@
--- Drop the existing foreign key constraint
-ALTER TABLE `social_accounts` DROP FOREIGN KEY `social_accounts_user_id_foreign`;
-
--- Add the new foreign key constraint that points to the correct table and column
-ALTER TABLE `social_accounts` 
-ADD CONSTRAINT `social_accounts_user_id_foreign` 
-FOREIGN KEY (`user_id`) REFERENCES `madkrapow_users` (`user_id`) 
-ON DELETE CASCADE; 
